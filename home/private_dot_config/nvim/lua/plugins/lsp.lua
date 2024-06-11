69103e5eba7080fb85b14ccc1df3fc1c6986812b
Seth Horsley <github.dvxhi@sl.isethi.com>
2024-06-11 14:51:03 +0200

    fix lazyvim update and add ruby-lsp

--- home/private_dot_config/nvim/lua/plugins/lsp.lua
+++ home/private_dot_config/nvim/lua/plugins/lsp.lua
@@ -8,8 +8,80 @@ return {
       -- disable a keymap
       keys[#keys + 1] = { "<leader>cc", false }
       -- add a keymap
-      keys[#keys + 1] = { "<leader>i", "<cmd>LspInfo<cr>", desc = "Show LSP info" }
+      keys[#keys + 1] = { "<leader>ci", "<cmd>LspInfo<cr>", desc = "Show LSP info" }
     end,
+
+    opts = {
+      diagnostics = { virtual_text = { prefix = "icons" } },
+      capabilities = {
+        workspace = {
+          didChangeWatchedFiles = {
+            dynamicRegistration = false,
+          },
+        },
+      },
+      ---@type lspconfig.options
+      servers = {
+        lua_ls = {
+          -- single_file_support = true,
+          settings = {
+            Lua = {
+              hover = { expandAlias = false },
+              type = {
+                castNumberToInteger = true,
+              },
+              diagnostics = {
+                disable = { "incomplete-signature-doc", "trailing-space" },
+                -- enable = false,
+                groupSeverity = {
+                  strong = "Warning",
+                  strict = "Warning",
+                },
+                groupFileStatus = {
+                  ["ambiguity"] = "Opened",
+                  ["await"] = "Opened",
+                  ["codestyle"] = "None",
+                  ["duplicate"] = "Opened",
+                  ["global"] = "Opened",
+                  ["luadoc"] = "Opened",
+                  ["redefined"] = "Opened",
+                  ["strict"] = "Opened",
+                  ["strong"] = "Opened",
+                  ["type-check"] = "Opened",
+                  ["unbalanced"] = "Opened",
+                  ["unused"] = "Opened",
+                },
+                unusedLocalExclude = { "_*" },
+              },
+            },
+          },
+        },
+        -- ruby_ls = {
+        --   mason = false,
+        --   cmd = { os.getenv("HOME") .. "/.asdf/shims/ruby-lsp" },
+        --   init_options = {
+        --     enabledFeatures = {
+        --       "codeActions",
+        --       "codeLens",
+        --       "completion",
+        --       "definition",
+        --       "documentHighlights",
+        --       "documentSymbols",
+        --       "foldingRanges",
+        --       "hover",
+        --       "inlayHint",
+        --       "selectionRanges",
+        --       "semanticHighlighting",
+        --       "workspaceSymbol",
+        --       -- "diagnostics",
+        --       -- "documentLink",
+        --       -- "formatting",
+        --       -- "onTypeFormatting",
+        --     },
+        --   },
+        -- },
+      },
+    },
   },
   {
     "williamboman/mason.nvim",
@@ -25,12 +97,31 @@ return {
         "typescript-language-server",
         "css-lsp",
         "standardrb",
-        "ruby-lsp",
         "terraform-ls",
         -- for python
         "black",
         "nextls",
       })
     end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { "dprint", { "prettierd", "prettier" } },
+        ["javascriptreact"] = { "dprint" },
+        ["typescript"] = { "dprint", { "prettierd", "prettier" } },
+        ["typescriptreact"] = { "dprint" },
+        ["ruby"] = { "standardrb" },
+      },
+      formatters = {
+        dprint = {
+          condition = function(_, ctx)
+            return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+          end,
+        },
+      },
+    },
   },
 }
