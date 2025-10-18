aa9a2c7df87d103577b212dc275c7b47753abd49
Seth Horsley <github.dvxhi@sl.isethi.com>
2025-10-18 19:46:09 -0400

    remove gh extensions

--- home/.chezmoiscripts/common/run_onchange_after_gh-extensions.sh
+++ home/.chezmoiscripts/common/run_onchange_after_gh-extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "==> 📜 Install gh extensions"
-
-gh extension install dlvhdr/gh-dash
-gh extension install seachicken/gh-poi
+# echo "==> 📜 Install gh extensions"
+#
+# gh extension install dlvhdr/gh-dash
+# gh extension install seachicken/gh-poi
