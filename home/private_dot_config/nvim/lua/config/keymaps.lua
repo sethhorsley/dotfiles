4b577bfe24cad40cf52d2a1d377940952ddb63c8
Seth Horsley <github.dvxhi@sl.isethi.com>
2025-10-18 14:02:49 -0400

    update dotfiles

--- home/private_dot_config/nvim/lua/config/keymaps.lua
+++ home/private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,45 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local discipline = require("config.discipline")
-discipline.cowboy()
+local keymap = vim.keymap
 
-local map = vim.keymap.set
---
--- -- tabufline
--- map("n", "<leader>b", "<cmd>enew<CR>", { desc = "Buffer New" })
---
--- map("n", "<leader>x", "<cmd>BufferLineCyclePrev<CR>", { desc = "Buffer Goto prev" })
-map("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, { desc = "Buffer Goto prev" })
-
--- map("n", "<S-tab>", function()
---   require("tabufline").prev()
--- end, { desc = "Buffer Goto prev" })
---
--- map("n", "<leader>x", function()
---   require("tabufline").close_buffer()
--- end, { desc = "Buffer Close" })
-
---
--- vim-tmux-navigator
-
-if os.getenv("TMUX") then
-  map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
-  map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
-  map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
-  map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
-end
-
--- delete other windows
-map("n", "<leader>wo", "<cmd>only<cr>", { desc = "Delete other windows" })
-
--- toggle tokyonight transparency
-map(
-  "n",
-  "<leader>tt",
-  "<cmd>lua require('config.colorscheme').toggle_transparency()<cr>",
-  { desc = "Toggle transparency" }
-)
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 vim.api.nvim_create_user_command("Cppath", function()
   local path = vim.fn.expand("%:.")
