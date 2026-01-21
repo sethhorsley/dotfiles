2a11c54fda354187c3c0f9e7288412538e649ab1
Seth Horsley <github.dvxhi@sl.isethi.com>
2026-01-21 11:12:17 -0500

    remove yabai and skhd

--- home/private_dot_config/skhd/executable_goto_last_space.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the number of the last space
-LAST_SPACE=$(yabai -m query --spaces | jq 'max_by(.index) | .index')
-
-# If not, focus on the last space
-yabai -m space --focus $LAST_SPACE
-
